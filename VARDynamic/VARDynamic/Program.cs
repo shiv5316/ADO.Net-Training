0719ff8923115400395757403101c0deb1f1361d
Shivansh Mishra <shivanshmishra209310@gmail.com>
2026-02-14 10:41:41 +0530

    ADO.NET Updated

---
+++ VARDynamic/VARDynamic/Program.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("VAR EXAMPLE....");
+//        var number = 10;
+//        Console.WriteLine("Value: " + number);
+//        Console.WriteLine("Type: " + number.GetType());
+//    }
+//}
+
+
+//using System;
+//class Program
+//{
+//    static void Main()
+//    {
+//        Func<int, bool> check = x => x > 2;
+//        Console.WriteLine(check(5));
+//        Console.WriteLine(check(1));
+//    }
+//}
+
+
